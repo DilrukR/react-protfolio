b36f4138a4096eb593ad43a7d84de18740a8ac38
DilrukR <68820399+DilrukR@users.noreply.github.com>
2022-07-24 19:18:07 +0530

    about-fix

--- src/component/About/about.jsx
+++ src/component/About/about.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./about.css";
 import ME from "../../Assest/me-about.jpg";
 import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const about = () => {
   return (
@@ -10,18 +12,41 @@ const about = () => {
       <h2>About Me</h2>
 
       <div className='container about-container'>
-        <div className='about_me'></div>
-
-        <div className='about__about_me_img'>
-          <img src={ME} alt='About me img' />
+        <div className='about_me'>
+          <div className='about_me-img'>
+            <img src={ME} alt='About me img' />
+          </div>
         </div>
 
         <div className='about_content'>
-          <div className='about_card'>
-            <FaAward className='about_icon' />
-            <h5>Experence</h5>
-            <small>3+ Years working</small>
+          <div className='about_cards'>
+            <article className='about_card'>
+              <FaAward className='about_icon' />
+              <h5>Experence</h5>
+              <small>3+ Years working</small>
+            </article>
+            <article className='about_card'>
+              <FiUsers className='about_icon' />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+            <article className='about_card'>
+              <VscFolderLibrary className='about_icon' />
+              <h5>Experence</h5>
+              <small>3+ Years working</small>
+            </article>
           </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis
+            maxime ratione quo maiores fugit ut dolores corrupti impedit
+            repudiandae tempora voluptate repellendus qui veritatis, quis
+            blanditiis voluptatibus perspiciatis corporis nam?
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>
+            Let's talk
+          </a>
         </div>
       </div>
     </section>
